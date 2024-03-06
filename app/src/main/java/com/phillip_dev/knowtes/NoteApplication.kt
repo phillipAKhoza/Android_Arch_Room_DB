b6e5096eadce6157cc188c02aad4749c46c84a53
phillip khoza <phillipantonk@gmail.com>
2024-03-06 16:05:12 +0200

    create db instance

--- app/src/main/java/com/phillip_dev/knowtes/NoteApplication.kt
+++ app/src/main/java/com/phillip_dev/knowtes/NoteApplication.kt
@@ -1,10 +1,14 @@
 package com.phillip_dev.knowtes
 
 import android.app.Application
+import com.phillip_dev.knowtes.Db.NoteDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
 class NoteApplication : Application(){
     val applicationScope = CoroutineScope(SupervisorJob())
 
+    val database by lazy {
+        NoteDatabase.getDataBase(this)
+    }
 }
