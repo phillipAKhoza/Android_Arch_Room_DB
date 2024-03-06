5103c80bf9061128bad3dcf3726bfd9363409b59
phillip khoza <phillipantonk@gmail.com>
2024-03-06 14:39:44 +0200

    add allNotes Var on repository class

--- app/src/main/java/com/phillip_dev/knowtes/Repository/NoteRepository.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Repository/NoteRepository.kt
@@ -1,7 +1,9 @@
 package com.phillip_dev.knowtes.Repository
 
 import com.phillip_dev.knowtes.Db.NoteDao
+import com.phillip_dev.knowtes.Model.Note
+import kotlinx.coroutines.flow.Flow
 
 class NoteRepository(private  val noteDao: NoteDao) {
-
+        val allNotes: Flow<List<Note>> = noteDao.getAllNotes()
 }
