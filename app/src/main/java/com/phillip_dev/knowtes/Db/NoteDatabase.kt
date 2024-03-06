c9a184032e63b898583185f6d82ceeb523c2593a
phillip khoza <phillipantonk@gmail.com>
2024-03-06 12:22:14 +0200

    inherit Note Db class from Room Db

--- app/src/main/java/com/phillip_dev/knowtes/Db/NoteDatabase.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Db/NoteDatabase.kt
@@ -1,8 +1,9 @@
 package com.phillip_dev.knowtes.Db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.phillip_dev.knowtes.Model.Note
 
 @Database(entities = [Note::class], version = 1)
-abstract class NoteDatabase {
+abstract class NoteDatabase: RoomDatabase() {
 }
