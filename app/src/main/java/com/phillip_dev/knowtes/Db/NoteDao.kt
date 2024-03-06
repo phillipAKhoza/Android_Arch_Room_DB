22d89b6fcec7cfe9447bb83c378eb509996e206c
phillip khoza <phillipantonk@gmail.com>
2024-03-06 12:09:19 +0200

    add flow to query fun to get allNotes on dao

--- app/src/main/java/com/phillip_dev/knowtes/Db/NoteDao.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Db/NoteDao.kt
@@ -6,7 +6,8 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.phillip_dev.knowtes.Model.Note
-import java.util.concurrent.Flow
+import kotlinx.coroutines.flow.Flow
+
 
 @Dao
 interface NoteDao {
@@ -21,7 +22,7 @@ interface NoteDao {
     suspend fun delete(note: Note)
 
     @Query("SELECT * FROM note_table ORDER BY id ASC")
-    fun getAllNotes() : List<Note>
+    fun getAllNotes() : Flow<List<Note>>
 
 
 }
