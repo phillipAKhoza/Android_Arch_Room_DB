48bdc7583eec184e00c04a677d4e53dbec3460b4
phillip khoza <phillipantonk@gmail.com>
2024-03-06 12:21:14 +0200

    annotate db to  Note Db class and assign data class

--- app/src/main/java/com/phillip_dev/knowtes/Db/NoteDatabase.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Db/NoteDatabase.kt
@@ -1,4 +1,8 @@
 package com.phillip_dev.knowtes.Db
 
+import androidx.room.Database
+import com.phillip_dev.knowtes.Model.Note
+
+@Database(entities = [Note::class], version = 1)
 class NoteDatabase {
 }
