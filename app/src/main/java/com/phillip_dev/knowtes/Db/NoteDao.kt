8ef8a377d1ad33d618f20ae54171d4056658575a
phillip khoza <phillipantonk@gmail.com>
2024-03-06 12:03:53 +0200

    add query fun to get allNotes to dao

--- app/src/main/java/com/phillip_dev/knowtes/Db/NoteDao.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Db/NoteDao.kt
@@ -3,8 +3,10 @@ package com.phillip_dev.knowtes.Db
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import com.phillip_dev.knowtes.Model.Note
+import java.util.concurrent.Flow
 
 @Dao
 interface NoteDao {
@@ -17,5 +19,9 @@ interface NoteDao {
 
     @Delete
     suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM note_table ORDER BY id ASC")
+    fun getAllNotes() : List<Note>
+
 
 }
