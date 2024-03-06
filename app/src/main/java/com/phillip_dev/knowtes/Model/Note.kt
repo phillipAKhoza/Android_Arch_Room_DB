4230011c619d847f078fa3484d8be6af4c01cb1a
phillip khoza <phillipantonk@gmail.com>
2024-03-06 11:55:27 +0200

    add primaryKey

--- app/src/main/java/com/phillip_dev/knowtes/Model/Note.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Model/Note.kt
@@ -1,7 +1,10 @@
 package com.phillip_dev.knowtes.Model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity("note_table")
 data class Note(val title: String, val description: String) {
+    @PrimaryKey(autoGenerate = true)
+    val id : Int =0
 }
