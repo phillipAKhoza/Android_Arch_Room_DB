6a448efa368318569230813aa63076e708880585
phillip khoza <phillipantonk@gmail.com>
2024-03-06 11:57:51 +0200

    add insert fun to dao

--- app/src/main/java/com/phillip_dev/knowtes/Db/NoteDao.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Db/NoteDao.kt
@@ -1,8 +1,13 @@
 package com.phillip_dev.knowtes.Db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import com.phillip_dev.knowtes.Model.Note
 
 @Dao
 interface NoteDao {
+
+    @Insert
+    suspend fun addNote(note: Note)
 
 }
