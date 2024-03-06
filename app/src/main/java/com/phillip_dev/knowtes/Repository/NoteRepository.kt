e33c22a4e888418d0d643fec8048e840189b19b7
phillip khoza <phillipantonk@gmail.com>
2024-03-06 14:41:02 +0200

    add work thread fun to add note on repository class

--- app/src/main/java/com/phillip_dev/knowtes/Repository/NoteRepository.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Repository/NoteRepository.kt
@@ -1,9 +1,15 @@
 package com.phillip_dev.knowtes.Repository
 
+import androidx.annotation.WorkerThread
 import com.phillip_dev.knowtes.Db.NoteDao
 import com.phillip_dev.knowtes.Model.Note
 import kotlinx.coroutines.flow.Flow
 
 class NoteRepository(private  val noteDao: NoteDao) {
         val allNotes: Flow<List<Note>> = noteDao.getAllNotes()
+
+    @WorkerThread
+    suspend fun addNote(note: Note){
+        noteDao.addNote(note)
+    }
 }
