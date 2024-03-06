4c7459277b644b451083cb8a9fb14fcf789784bf
phillip khoza <phillipantonk@gmail.com>
2024-03-06 17:56:24 +0200

    get all notes as live data

--- app/src/main/java/com/phillip_dev/knowtes/ViewModel/NoteViewModel.kt
+++ app/src/main/java/com/phillip_dev/knowtes/ViewModel/NoteViewModel.kt
@@ -1,9 +1,14 @@
 package com.phillip_dev.knowtes.ViewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.phillip_dev.knowtes.Model.Note
 import com.phillip_dev.knowtes.Repository.NoteRepository
 
 
 class NoteViewModel(private  val repository: NoteRepository): ViewModel() {
+
+    val allNote : LiveData<List<Note>> = repository.allNotes.asLiveData()
 
 }
