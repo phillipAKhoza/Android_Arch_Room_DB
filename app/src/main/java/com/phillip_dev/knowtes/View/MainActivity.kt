64bb79071a07207e1837a3c4f3420c0b785b4b82
phillip khoza <phillipantonk@gmail.com>
2024-03-06 18:16:35 +0200

    add view model on main

--- app/src/main/java/com/phillip_dev/knowtes/View/MainActivity.kt
+++ app/src/main/java/com/phillip_dev/knowtes/View/MainActivity.kt
@@ -2,11 +2,16 @@ package com.phillip_dev.knowtes.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
 import com.phillip_dev.knowtes.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: ViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val viewModelFactory =
     }
 }
