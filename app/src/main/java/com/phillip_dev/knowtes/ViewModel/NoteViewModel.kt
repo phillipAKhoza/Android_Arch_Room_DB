d08d0e2039dd99c521251892d0e78af893cffc3d
phillip khoza <phillipantonk@gmail.com>
2024-03-06 17:52:03 +0200

    inherit view model on viewModel class

--- app/src/main/java/com/phillip_dev/knowtes/ViewModel/NoteViewModel.kt
+++ app/src/main/java/com/phillip_dev/knowtes/ViewModel/NoteViewModel.kt
@@ -1,6 +1,9 @@
 package com.phillip_dev.knowtes.ViewModel
 
+import androidx.lifecycle.ViewModel
 import com.phillip_dev.knowtes.Repository.NoteRepository
 
-class NoteViewModel(private  val repository: NoteRepository) {
+
+class NoteViewModel(private  val repository: NoteRepository): ViewModel() {
+
 }
