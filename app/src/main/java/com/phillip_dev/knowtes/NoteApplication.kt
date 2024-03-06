1e0b1129953cdbe706466000d242547d0e610c1b
phillip khoza <phillipantonk@gmail.com>
2024-03-06 16:03:28 +0200

    add app scope

--- app/src/main/java/com/phillip_dev/knowtes/NoteApplication.kt
+++ app/src/main/java/com/phillip_dev/knowtes/NoteApplication.kt
@@ -1,7 +1,10 @@
 package com.phillip_dev.knowtes
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class NoteApplication : Application(){
+    val applicationScope = CoroutineScope(SupervisorJob())
 
 }
