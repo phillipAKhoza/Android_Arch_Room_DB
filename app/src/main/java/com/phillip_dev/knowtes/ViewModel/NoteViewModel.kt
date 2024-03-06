c68f07bcefd6b3601a6662f01b4d178e691ed817
phillip khoza <phillipantonk@gmail.com>
2024-03-06 18:02:14 +0200

    add addnot fun on viewModel class

--- app/src/main/java/com/phillip_dev/knowtes/ViewModel/NoteViewModel.kt
+++ app/src/main/java/com/phillip_dev/knowtes/ViewModel/NoteViewModel.kt
@@ -3,12 +3,19 @@ package com.phillip_dev.knowtes.ViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.phillip_dev.knowtes.Model.Note
 import com.phillip_dev.knowtes.Repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class NoteViewModel(private  val repository: NoteRepository): ViewModel() {
 
     val allNote : LiveData<List<Note>> = repository.allNotes.asLiveData()
+
+    fun addNote(note: Note)= viewModelScope.launch(Dispatchers.IO) {
+        repository.addNote(note)
+    }
 
 }
