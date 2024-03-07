142f1c6c2e8c839e56c3ed229a4a94269084ae81
phillip khoza <phillipantonk@gmail.com>
2024-03-07 16:37:25 +0200

    inherit recycler view from note adapter

--- app/src/main/java/com/phillip_dev/knowtes/Adapters/NoteAdapter.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Adapters/NoteAdapter.kt
@@ -1,14 +1,30 @@
 package com.phillip_dev.knowtes.Adapters
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.phillip_dev.knowtes.R
 
-class NoteAdapter {
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val title : TextView = itemView.findViewById(R.id.txt_title)
         val description : TextView = itemView.findViewById(R.id.txt_desc)
+        val cardView : CardView = itemView.findViewById(R.id.cardView)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        TODO("Not yet implemented")
     }
 }
