d81d0931421df0f26fd8d4bbe00675f08520a06c
phillip khoza <phillipantonk@gmail.com>
2024-03-07 16:38:38 +0200

    add empty array

--- app/src/main/java/com/phillip_dev/knowtes/Adapters/NoteAdapter.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Adapters/NoteAdapter.kt
@@ -5,9 +5,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.phillip_dev.knowtes.Model.Note
 import com.phillip_dev.knowtes.R
 
 class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    var notes: List<Note> = ArrayList()
 
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val title : TextView = itemView.findViewById(R.id.txt_title)
