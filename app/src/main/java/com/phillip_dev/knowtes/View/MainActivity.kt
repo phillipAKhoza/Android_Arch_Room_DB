0640a933abacf255ff74f5999193304ba2fa73e8
phillip khoza <phillipantonk@gmail.com>
2024-03-07 11:38:00 +0200

    add vm factory to note view model

--- app/src/main/java/com/phillip_dev/knowtes/View/MainActivity.kt
+++ app/src/main/java/com/phillip_dev/knowtes/View/MainActivity.kt
@@ -3,8 +3,10 @@ package com.phillip_dev.knowtes.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.phillip_dev.knowtes.NoteApplication
 import com.phillip_dev.knowtes.R
+import com.phillip_dev.knowtes.ViewModel.NoteViewModel
 import com.phillip_dev.knowtes.ViewModel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val viewModelFactory = NoteViewModelFactory((application as NoteApplication).repository)
+
+        noteViewModel = ViewModelProvider(this,viewModelFactory).get(NoteViewModel::class.java)
     }
 }
