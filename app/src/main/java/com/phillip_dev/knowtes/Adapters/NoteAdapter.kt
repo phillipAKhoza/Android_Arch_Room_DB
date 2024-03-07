eb6f5011f1f29030add38a6e6c22a1884cb44d2a
phillip khoza <phillipantonk@gmail.com>
2024-03-07 16:27:08 +0200

    adopter refactor

--- app/src/main/java/com/phillip_dev/knowtes/Adapters/NoteAdapter.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Adapters/NoteAdapter.kt
@@ -3,7 +3,7 @@ package com.phillip_dev.knowtes.Adapters
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class NoteAdaptor {
+class NoteAdapter {
 
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
