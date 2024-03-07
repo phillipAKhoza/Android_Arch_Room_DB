19fb384d5420c9d6ef58c57c0ee4398121cb8fb7
phillip khoza <phillipantonk@gmail.com>
2024-03-07 16:26:26 +0200

    add adopter

---
+++ app/src/main/java/com/phillip_dev/knowtes/Adapters/NoteAdaptor.kt
@@ -0,0 +1,9 @@
+package com.phillip_dev.knowtes.Adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdaptor {
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
