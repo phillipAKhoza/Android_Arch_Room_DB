e5a075594c77c54c3c5de11bc95ffc70e5da773a
phillip khoza <phillipantonk@gmail.com>
2024-03-07 16:34:36 +0200

    add variables to viewholder

--- app/src/main/java/com/phillip_dev/knowtes/Adapters/NoteAdapter.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Adapters/NoteAdapter.kt
@@ -1,9 +1,14 @@
 package com.phillip_dev.knowtes.Adapters
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.phillip_dev.knowtes.R
 
 class NoteAdapter {
 
-    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val title : TextView = itemView.findViewById(R.id.txt_title)
+        val description : TextView = itemView.findViewById(R.id.txt_desc)
+    }
 }
