160a7ed3f3d1c0985328bdab90742b49d069f7f9
phillip khoza <phillipantonk@gmail.com>
2024-03-08 10:27:06 +0200

    add note activity

---
+++ app/src/main/java/com/phillip_dev/knowtes/View/AddNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.phillip_dev.knowtes.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.phillip_dev.knowtes.R
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
