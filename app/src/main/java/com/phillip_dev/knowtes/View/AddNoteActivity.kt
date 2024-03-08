da205cdcf58926e22e1a68fec8377d21e90fd7fa
phillip khoza <phillipantonk@gmail.com>
2024-03-08 23:18:23 +0200

    adeclare late vars on AddNoteActivity

--- app/src/main/java/com/phillip_dev/knowtes/View/AddNoteActivity.kt
+++ app/src/main/java/com/phillip_dev/knowtes/View/AddNoteActivity.kt
@@ -2,9 +2,16 @@ package com.phillip_dev.knowtes.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.phillip_dev.knowtes.R
 
 class AddNoteActivity : AppCompatActivity() {
+
+    lateinit var txtTitle : EditText
+    lateinit var txtDescription : EditText
+    lateinit var btnCancel : Button
+    lateinit var btnSave : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
