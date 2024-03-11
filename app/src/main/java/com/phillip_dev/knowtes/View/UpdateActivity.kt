3754e5b05ba59727b7c9c915bdeba36e3f7e9abf
phillip khoza <phillipantonk@gmail.com>
2024-03-11 22:21:06 +0200

    add update note vars

--- app/src/main/java/com/phillip_dev/knowtes/View/UpdateActivity.kt
+++ app/src/main/java/com/phillip_dev/knowtes/View/UpdateActivity.kt
@@ -2,9 +2,15 @@ package com.phillip_dev.knowtes.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.phillip_dev.knowtes.R
 
 class UpdateActivity : AppCompatActivity() {
+    lateinit var txtTitle : EditText
+    lateinit var txtDescription : EditText
+    lateinit var btnCancel : Button
+    lateinit var btnSave : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update)
