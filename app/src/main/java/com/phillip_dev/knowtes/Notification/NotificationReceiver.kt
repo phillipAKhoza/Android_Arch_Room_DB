cea2eb39627019682c7f726ebad46f5bf657e7ee
phillip khoza <phillipantonk@gmail.com>
2024-03-12 18:19:57 +0200

    create notification channel with manager

--- app/src/main/java/com/phillip_dev/knowtes/Notification/NotificationReceiver.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Notification/NotificationReceiver.kt
@@ -16,6 +16,7 @@ class NotificationReceiver : BroadcastReceiver() {
             if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.O){
                 val channel = NotificationChannel(CHANNEL_ID,"1",NotificationManager.IMPORTANCE_DEFAULT)
                 val manager : NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                manager.createNotificationChannel(channel)
 
             }
         }
