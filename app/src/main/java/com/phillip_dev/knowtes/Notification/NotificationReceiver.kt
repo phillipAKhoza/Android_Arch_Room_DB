f1d5c323cb5853a2fa94b2ceec490766fc677fb9
phillip khoza <phillipantonk@gmail.com>
2024-03-12 18:12:29 +0200

    implement onReceive member

--- app/src/main/java/com/phillip_dev/knowtes/Notification/NotificationReceiver.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Notification/NotificationReceiver.kt
@@ -3,9 +3,13 @@ package com.phillip_dev.knowtes.Notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.core.app.NotificationCompat
 
 class NotificationReceiver : BroadcastReceiver() {
+    private  val CHANNEL_ID = "1"
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("Not yet implemented")
+        if (context != null){
+            val builder = NotificationCompat.Builder(context,CHANNEL_ID)
+        }
     }
 }
