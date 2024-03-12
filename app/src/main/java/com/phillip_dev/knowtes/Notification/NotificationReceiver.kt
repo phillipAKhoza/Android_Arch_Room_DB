54d25e9718eba52f17354c5ed9415694891c4a38
phillip khoza <phillipantonk@gmail.com>
2024-03-12 17:42:14 +0200

    inherit broadcast receiver and implement members

--- app/src/main/java/com/phillip_dev/knowtes/Notification/NotificationReceiver.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Notification/NotificationReceiver.kt
@@ -1,4 +1,11 @@
 package com.phillip_dev.knowtes.Notification
 
-class NotificationReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
 }
