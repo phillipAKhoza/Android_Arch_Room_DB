92ab9ee92ecca221a48991deef6da2c3e4761950
phillip khoza <phillipantonk@gmail.com>
2024-03-12 17:40:40 +0200

    implement addOnPositiveButtonClickListener set Toast

--- app/src/main/java/com/phillip_dev/knowtes/View/AddNoteActivity.kt
+++ app/src/main/java/com/phillip_dev/knowtes/View/AddNoteActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
 import com.phillip_dev.knowtes.Notification.NotificationReceiver
@@ -63,6 +64,8 @@ class AddNoteActivity : AppCompatActivity() {
 
                 val alarmManager : AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
                 alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,canceler.timeInMillis,AlarmManager.INTERVAL_DAY,pendingIntent)
+
+                Toast.makeText(applicationContext,"Note Reminder Set",Toast.LENGTH_LONG).show()
             }
         }
 
