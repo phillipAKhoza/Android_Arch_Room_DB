41104ab840f888a84219905be721724303c4ea12
phillip khoza <phillipantonk@gmail.com>
2024-03-12 18:14:32 +0200

    check if sdk is oreo

--- app/src/main/java/com/phillip_dev/knowtes/Notification/NotificationReceiver.kt
+++ app/src/main/java/com/phillip_dev/knowtes/Notification/NotificationReceiver.kt
@@ -3,6 +3,7 @@ package com.phillip_dev.knowtes.Notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
 
 class NotificationReceiver : BroadcastReceiver() {
@@ -10,6 +11,10 @@ class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (context != null){
             val builder = NotificationCompat.Builder(context,CHANNEL_ID)
+            if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.O){
+
+
+            }
         }
     }
 }
